8332c61310e924664ef5ea40d617ac14b1757ff1
kaizhang <zk65900931@gmail.com>
2018-10-05 12:28:47 -0700

    update

---
+++ compiler/Main.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
+import           Data.Monoid ((<>))
+import           Hakyll
+import Hakyll.Web.Sass (sassCompilerWith, sassDefConfig, SassOptions(..))
+
+saasOptions = sassDefConfig
+    { sassIncludePaths      = Just ["web/foundation/css"]
+    }
+
+--------------------------------------------------------------------------------
+main :: IO ()
+main = hakyll $ do
+    match "web/templates/*" $ compile templateBodyCompiler
+
+    match "web/static/img/*" $ do
+        route $ gsubRoute "web/" (const "")
+        compile copyFileCompiler
+
+    match "web/static/css/*" $ do
+        route $ gsubRoute "web/" (const "")
+        compile copyFileCompiler
+
+    match "web/static/js/*" $ do
+        route $ gsubRoute "web/" (const "")
+        compile copyFileCompiler
+
+    -- match "web/static/scss/app.scss" $ do
+    --    route $ constRoute "static/css/app.css"
+    --    compile $ sassCompilerWith saasOptions
+
+    match (fromList ["web/pages/install.rst"]) $ do
+        route $ composeRoutes (gsubRoute "web/pages/" $ const "") $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "web/templates/default.html" defaultContext
+            >>= relativizeUrls
+
+    match "web/pages/*.html" $ do
+        route $ gsubRoute "web/pages/" $ const ""
+        compile $ do
+            getResourceBody
+                >>= loadAndApplyTemplate "web/templates/default.html" defaultContext
+                >>= relativizeUrls
+
